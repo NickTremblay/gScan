fb844cb0e21b2f2aed2c0865ad3882f114f80e71
NickTremblay <nickt1@uri.edu>
2022-04-26 03:36:56 -0400

    Open context menu on separate thread to visualize points in realtime

--- main.cpp
+++ main.cpp
@@ -6,10 +6,96 @@
 #include <string>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <thread>
 #include "Plane/Plane.h"
 #include "Point/Point.h"
 #include "Line/Line.hpp"
 
+// Prompt user for input corresponding to context menu choice
+void addPointsFromContextChoice(unsigned int contextChoice, Plane* plane, bool* done, bool* listenForClicks){
+    // Append points to plane based on contextChoice
+    switch(contextChoice){
+        case 1:{
+            // Click on plane-
+            // Set window event listener for SFML window object on heap in main loop
+            // Instantiate point object using data from event object passed into handler
+            // Call w.addPoint(p)
+            // Listen for escape key to terminate event listener and call gScan driver
+            break;}
+            
+        case 2:{
+            // Manually enter
+            
+            // Init flag for valid coords
+            bool validCoords = false;
+            while(!validCoords){
+                unsigned int x;
+                std::cout << "Enter X:";
+                
+                // Validate x
+                try{
+                    std::cin >> x;
+                    
+                }catch(const std::exception&){
+                    std::cout << "Invalid x coord try again" << std::endl;
+                    continue;
+                }
+                
+                if(x > plane->width){
+                    std::cout << "Invalid x coord try again" << std::endl;
+                    continue;
+                }
+                
+                unsigned int y;
+                std::cout << "Enter Y:";
+                // Validate y
+                try{
+                    std::cin >> y;
+                    
+                }catch(const std::exception&){
+                    std::cout << "Invalid y coord try again" << std::endl;
+                    continue;
+                }
+                
+                if(y > plane->height){
+                    std::cout << "Invalid y coord try again" << std::endl;
+                    continue;
+                }
+                
+                // Declare new point on p and add to plane
+                Point* p = new Point(x, y, "#000000");
+                plane->addPoint(p);
+                
+                // Prompt user for another coord
+                char enterAnotherCoord;
+                while(true){
+                    // Overridden if invalid x or y
+                    std::cout << "Enter another coord(y/n)? ";
+                    
+                    try{
+                        std::cin >> enterAnotherCoord;
+                    }catch(const std::exception&){
+                        std::cout << std::endl;
+                        continue;
+                    }
+                    
+                    if(enterAnotherCoord != 'y' && enterAnotherCoord != 'n'){
+                        std::cout << std::endl;
+                        continue;
+                    }else break;
+                }
+                
+                if(enterAnotherCoord == 'n') break;
+            }
+            break;}
+            
+        case 3: {
+            // Enter from file-
+            
+            break;
+        }
+    }
+}
 
 int main(){
     //////////////////////////////////
@@ -42,6 +128,7 @@ int main(){
             std::cin >> contextChoice;
             std::cout << std::endl;
         }catch(const std::exception&){
+            std::cout << "catch" << std::endl;
             std::cout << "Invalid choice. Choose number from 1-3:";
             continue;
         }
@@ -50,94 +137,20 @@ int main(){
             validContextChoice = true;
             break;
         }else{
+            std::cout << "contextChoice out of bounds" << std::endl;
             std::cout << "Invalid choice. Choose number from 1-3:";
         }
     }
     
+    // Init plane object with default windowSize
     Plane plane(500, 500);
     
-    // Append points to plane based on contextChoice
-    switch(contextChoice){
-        case 1:{
-            // Click on plane-
-            // Set window event listener for SFML window object on heap in main loop
-            // Instantiate point object using data from event object passed into handler
-            // Call w.addPoint(p)
-            // Listen for escape key to terminate event listener and call gScan driver
-            break;}
-            
-        case 2:{
-            // Manually enter
-            
-            // Init flag for valid coords
-            bool validCoords = false;
-            while(!validCoords){
-                unsigned int x;
-                std::cout << "Enter X:";
-                
-                // Validate x
-                try{
-                    std::cin >> x;
-                    
-                }catch(const std::exception&){
-                    std::cout << "Invalid x coord try again" << std::endl;
-                    continue;
-                }
-                
-                if(x > plane.width){
-                    std::cout << "Invalid x coord try again" << std::endl;
-                    continue;
-                }
-                
-                unsigned int y;
-                std::cout << "Enter Y:";
-                // Validate y
-                try{
-                    std::cin >> y;
-                    
-                }catch(const std::exception&){
-                    std::cout << "Invalid y coord try again" << std::endl;
-                    continue;
-                }
-                
-                if(y > plane.height){
-                    std::cout << "Invalid y coord try again" << std::endl;
-                    continue;
-                }
-                
-                // Declare new point on p and add to plane
-                Point* p = new Point(x, y, "#000000");
-                plane.addPoint(p);
-                
-                // Prompt user for another coord
-                char enterAnotherCoord;
-                while(true){
-                    // Overridden if invalid x or y
-                    std::cout << "Enter another coord(y/n)? ";
-                    
-                    try{
-                        std::cin >> enterAnotherCoord;
-                    }catch(const std::exception&){
-                        std::cout << std::endl;
-                        continue;
-                    }
-                    
-                    if(enterAnotherCoord != 'y' && enterAnotherCoord != 'n'){
-                        std::cout << std::endl;
-                        continue;
-                    }else break;
-                }
-                
-                if(enterAnotherCoord == 'n') break;
-            }
-            break;}
-            
-        case 3: {
-            // Enter from file-
-            
-            break;
-        }
-    }
+    // Init flags for feedback from contextMenuThread
+    bool contextDone = false;
+    bool listenForClicks = (contextChoice == 1);
+    
+    // Launch remainder of contextMenu on new thread
+    std::thread contextThread(addPointsFromContextChoice, contextChoice, &plane, &contextDone, &listenForClicks);
     
     // Init SFML window
     sf::RenderWindow window(sf::VideoMode(plane.width, plane.height), "gScan");
@@ -154,8 +167,10 @@ int main(){
         while (window.pollEvent(event))
         {
             // Handle window close
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed){
                 window.close();
+                break;
+            }
         }
         
         // Set background color and clear previous frame
